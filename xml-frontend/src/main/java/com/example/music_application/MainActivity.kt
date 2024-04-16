0ed729dc2f738d12eeebd9081b7fdbc9b856f800
Fireentity <croceclaudio57@gmail.com>
2024-04-16 11:18:32 +0200

    feat: recycler view

--- xml-frontend/src/main/java/com/example/music_application/MainActivity.kt
+++ xml-frontend/src/main/java/com/example/music_application/MainActivity.kt
@@ -1,40 +1,23 @@
 package com.example.music_application
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.music_application.databinding.ActivityMainBinding
+import com.example.music_application.databinding.ContentMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        setContentView(R.layout.content_main)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return super.onSupportNavigateUp()
     }
 }
