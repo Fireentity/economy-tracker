0b9c2c5926b34e92128c641b29d65205c0157cd1
Fireentity <croceclaudio57@gmail.com>
2024-04-24 15:02:20 +0200

    feat: dialog for filtering transactions

--- xml-frontend/src/main/java/it/unipd/dei/music_application/ui/RegisterFragment.kt
+++ xml-frontend/src/main/java/it/unipd/dei/music_application/ui/RegisterFragment.kt
@@ -5,15 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import it.unipd.dei.music_application.R
 
-class RegisterFragment: Fragment() {
+
+class RegisterFragment : Fragment() {
+    private val items = arrayOf("Option 1", "Option 2", "Option 3")
+    private val selectedItems = BooleanArray(items.size) { false }
+    private val dialog = MaterialAlertDialogBuilder(requireContext())
+        .setMultiChoiceItems(items, selectedItems)  { _, which, isChecked ->
+            selectedItems[which] = isChecked
+        }
+        .create()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false);
+        val topBarButton: View? = view.findViewById(R.id.filter)
+
+        topBarButton?.setOnClickListener {
+            dialog.show()
+        }
+
+        return view
     }
 }
