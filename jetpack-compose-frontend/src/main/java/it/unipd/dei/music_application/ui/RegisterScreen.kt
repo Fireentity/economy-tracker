43188233ee1a3060807b4a38d4fc9592d89d1e0c
Soyadrul <marco.pavanetto02@gmail.com>
2024-04-26 17:44:56 +0200

    feat: top app bar

--- jetpack-compose-frontend/src/main/java/it/unipd/dei/music_application/ui/RegisterScreen.kt
+++ jetpack-compose-frontend/src/main/java/it/unipd/dei/music_application/ui/RegisterScreen.kt
@@ -2,18 +2,77 @@ package it.unipd.dei.music_application.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 
+data class TabItem(
+    val title: String
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    //val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    val tabItems = listOf(
+        TabItem(title = "Tutti"),
+        TabItem(title = "Uscite"),
+        TabItem(title = "Entate"),
+    )
+
+    Surface(
+        modifier = modifier
     ) {
-        Text(text = "Registro")
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text(
+                            "Page title",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    //scrollBehavior = scrollBehavior,
+                )
+            },
+        ) {
+            paddingValues ->
+            Column(
+                modifier.padding(paddingValues)
+            ) {
+
+            }
+        }
     }
+
 }
