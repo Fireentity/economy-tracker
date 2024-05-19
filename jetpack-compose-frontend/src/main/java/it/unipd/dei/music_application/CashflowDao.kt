953a4c44710fd408cb876a43f58c4a3b1d87cb0c
Soyadrul <marco.pavanetto02@gmail.com>
2024-05-19 12:27:18 +0200

    feat: added the ability to remove a query

--- jetpack-compose-frontend/src/main/java/it/unipd/dei/music_application/CashflowDao.kt
+++ jetpack-compose-frontend/src/main/java/it/unipd/dei/music_application/CashflowDao.kt
@@ -1,6 +1,7 @@
 package it.unipd.dei.music_application
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
@@ -10,6 +11,9 @@ interface CashflowDao {
     @Upsert
     suspend fun insertMoneyMovement(cashflow: Cashflow)
 
+    @Delete
+    suspend fun deleteMoneyMovement(cashflow: Cashflow)
+
     @Query("SELECT * FROM Cashflow ORDER BY id ASC")
-    fun getAllCashflow(): Flow<List<Cashflow>>
+    fun getAllCashflowOrderedById(): Flow<List<Cashflow>>
 }
