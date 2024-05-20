0ae85610fd6d351e48c59c7eb1d65ee13387e196
Soyadrul <marco.pavanetto02@gmail.com>
2024-05-20 15:40:45 +0200

    chore: removed ViewModel and database related files

--- jetpack-compose-frontend/src/main/java/it/unipd/dei/music_application/CashflowState.kt
+++
@@ -1,8 +0,0 @@
-package it.unipd.dei.music_application
-
-data class CashflowState(
-    val cashflows: List<Cashflow> = emptyList(),
-    val amount: Double = 0.0,
-    val isAddingCashflow: Boolean = false,
-    val sortType: SortType = SortType.AMOUNT
-)
