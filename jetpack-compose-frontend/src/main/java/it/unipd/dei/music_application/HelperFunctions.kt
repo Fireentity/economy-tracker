88c6dbdd504abd58e569edf689d2a6b8e05b98dc
Soyadrul <marco.pavanetto02@gmail.com>
2024-05-31 19:45:30 +0200

    chore: modified the display of movements

--- jetpack-compose-frontend/src/main/java/it/unipd/dei/music_application/HelperFunctions.kt
+++ jetpack-compose-frontend/src/main/java/it/unipd/dei/music_application/HelperFunctions.kt
@@ -1,6 +1,9 @@
 package it.unipd.dei.music_application
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,6 +12,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -18,6 +22,7 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Date
+import it.unipd.dei.music_application.R
 
 @Composable
 fun MyDivider() {
@@ -45,21 +50,36 @@ fun DisplayMovements(movements: State<List<MovementWithCategory>>, modifier: Mod
     LazyColumn(
         modifier = modifier
     ) {
-        val textModifier = Modifier.padding(16.dp)
+        //val textModifier = Modifier.padding(16.dp)
 
         items(movements.value.size) { index ->
             Row(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 //val date: Date = movements.value.get(index).movement.updatedAt
                 val instant = Instant.ofEpochMilli(movements.value.get(index).movement.updatedAt)
                 val dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
-                val formatter = DateTimeFormatter.ofPattern("dd/mm/yyyy")
-                Text(text = "${dateTime.format(formatter)}")
-                Text("${movements.value.get(index).category.toString()}")
+                val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+
+                /*Image(
+                    painter = painterResource(it.unipd.dei.music_application.R.drawable.),
+                    contentDescription = "Circle Up",
+                    modifier = Modifier.size(100.dp),
+                    contentScale = ContentScale.Crop
+                )*/
+
+                Column {
+                    Text(text = dateTime.format(formatter))
+                    Text(text = movements.value.get(index).category.toString())
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+                
                 Text(
-                    text = "€${movements.value.get(index).movement.amount}",
-                    textAlign = TextAlign.Right
+                    text = "€${movements.value.get(index).movement.amount}"
                 )
             }
             
