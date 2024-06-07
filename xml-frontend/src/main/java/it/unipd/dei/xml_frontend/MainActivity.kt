595cda210cdf2aa886e41985de5acb30f8f26993
Fireentity <croceclaudio57@gmail.com>
2024-06-07 16:12:24 +0200

    feat: initial commit

---
+++ xml-frontend/src/main/java/it/unipd/dei/xml_frontend/MainActivity.kt
@@ -0,0 +1,11 @@
+package it.unipd.dei.xml_frontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        R.layout.test
+    }
+}
